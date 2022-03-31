a3526f7a9c86de1deaf5d7f7660136f9330a37d5
Julian-Lop <you@example.com>
2022-03-31 18:21:26 -0500

    rutas-creadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ItemsComponent } from './components/items/items.component';
 import { ItemComponent } from './components/item/item.component';
 import { TotalComponent } from './components/total/total.component';
+import { AdditemComponent } from './components/additem/additem.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,14 @@ import { TotalComponent } from './components/total/total.component';
     HeaderComponent,
     ItemsComponent,
     ItemComponent,
-    TotalComponent
+    TotalComponent,
+    AdditemComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
